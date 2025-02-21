41f5012638834f233c97c28b037f9ab7e0e83bc0
emersonbtsilva <emerson_bsilva1@hotmail.com>
2025-02-21 20:18:57 -0300

    Aula Java - Iniciando com Spring Boot

---
+++ aulas-java/src/main/java/com/fiap/ecr/aulas_java/AulasJavaApplication.java
@@ -0,0 +1,14 @@
+package com.fiap.ecr.aulas_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulasJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulasJavaApplication.class, args);
+	}
+
+}
+
---
+++ aulas-java/src/test/java/com/fiap/ecr/aulas_java/AulasJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.ecr.aulas_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulasJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
